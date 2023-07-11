25f7e3313c677d2b95dd489fd1ac5a80b3a21249
A.Yassine <95106484+a5medHub@users.noreply.github.com>
2023-07-11 16:46:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="index.css">
-    </head>
-    <body>
-        <section class="first">
-            <div>
-                <h1>Metric/Imperial Unit Converter</h1>
-                <!-- <p id="digit"></p> -->
-                <form>
-                    <label for="cnumber"></label>
-                    <input type="text" name="cnumber" class="digit" id="convert-number" value="">
-                </form>
-                <!-- <input class="digit" value=""> -->
-                <button id="convert-btn">convert</button>
-            </div>
-        </section>
-        <section class="second">
-            <div class="length">
-                <h3>Lenght (Meter/Feet)</h3>
-                <p id="lenght"></p>
-            </div>
-            <div class="volume">
-                <h3>Volume (Liters/Gallons)</h3>
-                <p id="volume"></p>
-            </div>
-            <div class="mass">
-                <h3>Mass (Kilograms/Pounds)</h3>
-                <p id="mass"></p>
-            </div>
-        </section>
-        <script src="index.js"></script>
-    </body>
-</html>
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+document.getElementById("convert-number").value = 0
+let convertNumber = 0
+const lenght = document.getElementById("lenght")
+const volume = document.getElementById("volume")
+const mass = document.getElementById("mass")
+const convertBtn = document.getElementById("convert-btn")
+let MTF = FTM = LTG = GTL = KTP = PTK = 0
+
+convertBtn.addEventListener("click",function(){
+    convertNumber = document.getElementById("convert-number").value
+    
+    MTF = Number(convertNumber)*3.281
+    FTM = Number(convertNumber)/3.281
+    
+    LTG = Number(convertNumber)*0.264
+    GTL = Number(convertNumber)/0.264
+    
+    KTP = Number(convertNumber)*2.204
+    PTK = Number(convertNumber)/2.204
+    
+     converted()
+})
+function converted(){
+    convertNumber = document.getElementById("convert-number").value
+    lenght.innerText = 
+    `${convertNumber} Meter = ${MTF.toFixed(3)} feet | ${convertNumber} feet = ${FTM.toFixed(3)} meter `
+     
+    volume.innerText = 
+    `${convertNumber} Liters = ${LTG.toFixed(3)} Gallons | ${convertNumber} Gallons = ${GTL.toFixed(3)} Liters `
+     
+    mass.innerText = 
+    `${convertNumber} Kilograms = ${KTP.toFixed(3)} Pounds |  ${convertNumber} Pounds = ${PTK.toFixed(3)} Kilograms `
+    
+    // console.log("converted")
+    
+}
+converted()
