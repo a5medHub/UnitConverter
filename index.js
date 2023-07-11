b4dcb5434d827b658e02e5e2ab548a80290f1140
A.Yassine <95106484+a5medHub@users.noreply.github.com>
2023-07-11 16:46:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <section class="first">
+            <div>
+                <h1>Metric/Imperial Unit Converter</h1>
+                <!-- <p id="digit"></p> -->
+                <form>
+                    <label for="cnumber"></label>
+                    <input type="text" name="cnumber" class="digit" id="convert-number" value="">
+                </form>
+                <!-- <input class="digit" value=""> -->
+                <button id="convert-btn">convert</button>
+            </div>
+        </section>
+        <section class="second">
+            <div class="length">
+                <h3>Lenght (Meter/Feet)</h3>
+                <p id="lenght"></p>
+            </div>
+            <div class="volume">
+                <h3>Volume (Liters/Gallons)</h3>
+                <p id="volume"></p>
+            </div>
+            <div class="mass">
+                <h3>Mass (Kilograms/Pounds)</h3>
+                <p id="mass"></p>
+            </div>
+        </section>
+        <script src="index.js"></script>
+    </body>
+</html>
